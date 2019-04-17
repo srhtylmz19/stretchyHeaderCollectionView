290d82ca666f94dacc2034480d49a4ad21194ffe
serhat yılmaz <srhtylmz19@gmail.com>
2019-04-17 14:59:36 +0300

    Initial Commit

---
+++ stretchyHeaderCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stretchyHeaderCollectionView
+//
+//  Created by Serhat Yılmaz on 17.04.2019.
+//  Copyright © 2019 Serhat Yılmaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
